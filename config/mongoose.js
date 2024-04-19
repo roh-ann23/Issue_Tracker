3da3d44ead5a7791601d1f741ced3be0bd31f0a8
roh-ann23 <158121284+roh-ann23@users.noreply.github.com>
2024-04-19 17:01:09 +0530

    commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config();
+// const dotenv = require('dotenv');
+// dotenv.config();
 
-mongoose.connect(`${process.env.MONGO_URL}`);
+// mongoose.connect(`${process.env.MONGO_URL}`);
+const MONGO_URL = 'mongodb+srv://rohanjadhavronny04:K6eJTymHKYtED1o7@cluster0.xzxuatf.mongodb.net/IssueTracker';
+
+mongoose.connect(MONGO_URL)
 
 const db = mongoose.connection;
+
 db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));   // K6eJTymHKYtED1o7
 
 db.once('open', () => {
