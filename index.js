cfd2c436062af2f2137ceae974371037a8f2016d
roh-ann23 <158121284+roh-ann23@users.noreply.github.com>
2024-04-19 16:07:17 +0530

    small changes

--- index.js
+++ index.js
@@ -19,9 +19,10 @@ app.set('views', './views');
 
 // use express router
 app.use('/', require('./routes'));
-app.listen(port, (err) => {
-  if (err) {
-    console.log(`Error in running the server : ${err}`);
-  }
+
+app.listen(port, () =>{
+  // if (err) {
+  //   console.log(`Error in running the server : ${err}`);
+  // }
   console.log(`Server is running on the port: ${port}`);
 });
